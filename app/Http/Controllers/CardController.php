69f879a9639acbd519f43d06631246ca0dc374f7
Halil Ibrahim Ozkul <ibrahimozkul@gmail.com>
2023-09-01 13:42:42 +0300

    cards crud

--- app/Http/Controllers/CardController.php
+++ app/Http/Controllers/CardController.php
@@ -2,11 +2,46 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Card;
 use Illuminate\Http\Request;
 
 class CardController extends Controller
 {
     public function getData() {
+        return Card::all();
+    }
+    public function addData(Request $request) {
 
+        $card = new Card();
+        $card->title = $request->title;
+        $card->container = $request->container;
+        $card->created_at = now();
+        $card->updated_at = now();
+        $result = $card->save();
+
+        if($result) {
+            return ['Status'=>'Success'];
+        }
+        return ['Status'=>'Error'];
+    }
+
+    public function updateData(Request $request) {
+        $card = Card::find($request->id);
+        $card->title = $request->title;
+        $card->container = $request->container;
+        $result = $card->save();
+        if($result) {
+            return ['Status'=>'Success'];
+        }
+        return ['Status'=>'Error'];
+    }
+
+    public function deleteData($id) {
+        $card = Card::find($id);
+        $result = $card->delete();
+        if($result) {
+            return ['Status'=>'Success'];
+        }
+        return ['Status'=>'Error'];
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\CardController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +18,8 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('get_data',[CardController::class, 'getData']);
+Route::post('add_data',[CardController::class, 'addData']);
+Route::put('update_data',[CardController::class, 'updateData']);
+Route::delete('delete_data/{id}',[CardController::class, 'deleteData']);
